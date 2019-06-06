a1b5711936c522d73fdffc9f148f96a483ae9869
Daianes26 <daianeasilva26@gmail.com>
2019-06-06 08:56:33 -0400

    Replaced words in list.. Need to count them

--- word_frequency.py
+++ word_frequency.py
@@ -1,13 +1,46 @@
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
-    'will', 'with'
+    'will', 'with',
 ]
-
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
+
+    # Use with open to read the file
+    with open(file) as source_file:
+          source_file = source_file.read()
+          source_file = clean_text(source_file)
+          print(source_file)
+
+    
+
+def clean_text(text):
+    """
+    Remove punctuation from string
+    """
+    text = text.replace(",", "").replace("!", "").replace(".", "").replace("`", "")
+    text = text.lower()
+    text = text.split(" ")
+    for word in list(text):
+        if word in STOP_WORDS:
+            print(word)
+            text.remove(word)
+                    
+    return text
+
+
+
+def count_word_freq(text):
+    """
+    Count how many times word have been used
+    """
     pass
+
+
+
+
+
 
 
 if __name__ == "__main__":
