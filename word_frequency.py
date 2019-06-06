c4eceb5f0378f1c0f2e8e57523f94d5895d783ef
Daianes26 <daianeasilva26@gmail.com>
2019-06-06 17:10:36 -0400

    Finished assignment

--- word_frequency.py
+++ word_frequency.py
@@ -4,45 +4,38 @@ STOP_WORDS = [
     'will', 'with',
 ]
 
+#function is called in the end. It works on both files. This is why file is called first.
 def print_word_freq(file):
-    """Read in `file` and print out the frequency of words in that file."""
-
-    # Use with open to read the file
     with open(file) as source_file:
-          source_file = source_file.read()
-          source_file = clean_text(source_file)
-          print(source_file)
+        #create a variable
+        text_file = source_file.read()
+        # print(text_file)
 
-    
+        text_file_rem_punc= text_file.replace(",", "").replace("!", "").replace(".", "").replace("`", "").replace("--","")
+        # print(text_file_rem_punc)
+        text_lower_space = text_file_rem_punc.lower().split(" ")
+        # print(text_lower_space)
 
-def clean_text(text):
-    """
-    Remove punctuation from string
-    """
-    text = text.replace(",", "").replace("!", "").replace(".", "").replace("`", "")
-    text = text.lower()
-    text = text.split(" ")
-    for word in list(text):
+    #loop through list and remove bad words
+    for word in list (text_lower_space):
         if word in STOP_WORDS:
-            print(word)
-            text.remove(word)
-                    
-    return text
+            text_lower_space.remove(word)
+            
+    # print(text_lower_space)
 
+    #loop through list and create a dictionary
+    dict_text ={}
+    for word in text_lower_space:
+        if word not in dict_text.keys():
+            dict_text[word] = 1
+        else:
+            dict_text[word] +=1
 
+    sorted_words= sorted(dict_text.items(), key=lambda x: x[1], reverse=True)
+    for word, frequency in sorted_words[0:10]:
+        print(word, "|", frequency, "*" * frequency)
 
-def count_word_freq(text):
-    """
-    Count how many times word have been used
-    """
-    pass
-
-
-
-
-
-
-
+#End of code I worked on. 
 if __name__ == "__main__":
     import argparse
     from pathlib import Path
